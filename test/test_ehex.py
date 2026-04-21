3eb08c3c79760238396f727684432cd20f1b3aa0
tolcreator <tolcreator@yahoo.co.uk>
2026-04-21 09:41:16 +0100

    Found out how to parametrize exception testing.

--- test/test_ehex.py
+++ test/test_ehex.py
@@ -1,42 +1,38 @@
 """ For testing src/utils/ehex.py """
 
-import src.utils.ehex as ehex
+from contextlib import nullcontext
 import pytest
+import src.utils.ehex as ehex
+
 
 @pytest.mark.parametrize("test_value, expected",
-    [ ('0', 0), ('9', 9), ('A', 10), ('G', 16) ])
+    [ ('0', nullcontext(0)), 
+      ('9', nullcontext(9)), 
+      ('A', nullcontext(10)), 
+      ('G', nullcontext(16)),
+      ( 1 , pytest.raises(TypeError)),
+      ('a', pytest.raises(ValueError))
+     ])
 def test_hex_to_int(test_value, expected):
-    assert ehex.hex_to_int(test_value) == expected
-
-def test_hex_to_int_value_error():
-    with pytest.raises(ValueError):
-        ehex.hex_to_int("Something invalid")
-
-def test_hex_to_int_type_error():
-    with pytest.raises(TypeError):
-        """ Looks sort of right, but it isn't """
-        ehex.hex_to_int(1)
+    with expected as e:
+        assert ehex.hex_to_int(test_value) == e
 
 @pytest.mark.parametrize("test_value, expected",
-    [ (0, '0'), (8, '8'), (11, 'B'), (17, 'H') ])
+    [ (  0, nullcontext('0')), 
+      (  9, nullcontext('9')), 
+      ( 10, nullcontext('A')), 
+      ( 16, nullcontext('G')),
+      ('1', pytest.raises(TypeError)),
+      ( 51, pytest.raises(ValueError))
+     ])
 def test_int_to_hex(test_value, expected):
-    assert ehex.int_to_hex(test_value) == expected
-
-def test_int_to_hex_value_error():
-    with pytest.raises(ValueError):
-        """ Out of bounds """
-        ehex.int_to_hex(51)
-
-def test_int_to_hex_type_error():
-    with pytest.raises(TypeError):
-        """ Looks sort of right, but it isn't """
-        ehex.int_to_hex('2')
+    with expected as e:
+        assert ehex.int_to_hex(test_value) == e
 
 @pytest.mark.parametrize("test_value, expected",
     [ ('0', True), ('9', True), ('B', True), ('X', True), 
       ( 0, False), ( 0.1, False), ("Something invalid", False) ])
 def test_is_valid(test_value, expected):
     assert ehex.is_valid(test_value) == expected
-
 
 
